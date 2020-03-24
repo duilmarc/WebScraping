180380e6fdfeb258ebac08a3ba070fb97c0d14b8
duilmarc <franco.chp@gmail.com>
2020-03-23 19:07:36 -0500

    añadida la funcion de parseo
    Funcion para buscar titutlos de acuerdo a un termino en especifico

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
+
 const moongose = require("mongoose");
 const { MONGO_URI } = require('./config');
 const axios = require('axios').default;
 const cheerio = require('cheerio');
 const con = require("node-cron");
 const { BreakingNew} = require("./models");
+
+//Conexion a la base se datos 
 moongose.connect(MONGO_URI , {useNewUrlParser : true});
 
+function validar( breakingNew, termino)
+{
+    if( breakingNew.title.indexOf(termino) !== 1 )
+        console.log(title);
+    else
+        continue;
+}
+
+function parseo( texto , termino){
+    var valor = texto.toLowerCase().indexOf(termino.toLowerCase());
+    return valor;
+}
+// Se ejecuta cada segundo * * * * * * - segundo - minuto - hora - dia - mes - dia del mes 
 con.schedule( "* * * * * *" , async () => {
     console.log("Cron Job executed");
     const html = await axios.get("https://cnnespanol.cnn.com/");
     const $ = cheerio.load(html.data);
     const titles = $(".news__title");
+    var termino = 'Coronavirus';
 
     titles.each((index , element) => {
         const breakingNew = {
@@ -21,7 +38,9 @@ con.schedule( "* * * * * *" , async () => {
                     .children()
                     .attr("href")
         };
-        BreakingNew.create([breakingNew]);
+        if( parseo(breakingNew.title, termino)  !== -1){
+            BreakingNew.create([breakingNew]);
+        }
 
     });
 });
